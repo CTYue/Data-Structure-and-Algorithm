4b87769a881f0fb6b61c5eb20b1d93e77449ec91
CTY <chitung.yue@gmail.com>
2019-03-05 22:12:32 -0500

    Changes to be committed: 	new file:   Remove_Nth_Node_From_End_of_List/remove_nth_node_from_end_of_list.cpp 	new file:   Stack/Stack_Using_Queues.cpp

---
+++ Remove_Nth_Node_From_End_of_List/remove_nth_node_from_end_of_list.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+//Definition for singly-linked list.
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) 
+    {
+        if(head==nullptr)
+            return nullptr;
+        
+        //删去尾部第N个节点
+        ListNode* temp;
+        
+        //正向遍历还是反向遍历？
+        //单链表
+        //如果正向遍历，那么时间复杂度会变大
+        while(head!=nullptr)
+        {
+            
+        }
+        
+        return temp;
+    }
+};
---
+++ Stack/Stack_Using_Queues.cpp
@@ -0,0 +1,53 @@
+//
+//  Stack_Using_Queues.cpp
+//  
+//
+//  Created by Zidong Yu on 2/21/19.
+//
+
+#include <iostream>
+#include <queue>
+
+class MyStack
+{
+public:
+    MyStack(){}
+
+    void push()
+    {
+
+    }
+    
+    int pop()
+    {
+
+    } 
+
+    int top()
+    {
+
+    }
+
+    bool empty()
+    {
+
+
+        
+    }
+
+
+private:
+    std::queue<int> q_main;
+    std::queue<int> q_top;//?
+
+};
+
+int main()
+{
+
+
+
+
+
+    return 0;
+}
