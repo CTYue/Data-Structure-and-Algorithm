a1fd14432d229a72ae46864a0c8951ff1eb635bc
CTY <chitung.yue@gmail.com>
2019-06-05 23:57:48 -0400

    Updated solution in 95_Unique_Binary_Search_Trees_II.cpp.

--- 95_Unique_Binary_Search_Trees_II.cpp
+++ 95_Unique_Binary_Search_Trees_II.cpp
@@ -1,57 +1,36 @@
-/*
- * @Author: Zidong Yu
- * @Email: chitung.yue@gmail.com
- * @Date: 2019-06-05 14:38:19
- * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-06-05 18:50:29
- * @Description: Solution(s) for Leetcode #95.
- */
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+class TreeNode 
+{
+public:
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+private:
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+};
+
 class Solution {
 public:
     
-    //Dynamic Programming Approach
-    //Time Complexity: O(n^2)
-    //Space Complexity: O(n)
-    int numTrees(int n) 
-    {
-        if(n<=2) return n;
+    //Recursion Approach
+    //Time Complexity:
+    //Space Complexity:
+    vector<TreeNode*> generateTrees(int n) 
+    {        
         
-        std::vector<int> dp(n+1,0);        
-        dp[0]=1;
-        dp[1]=1;
-        
-        for(int i=2;i<=n;++i)
-        {
-            for(int j=1;j<=i;++j)
-            {
-                //Pick j as the root
-                dp[i]+=dp[j-1]*dp[i-j];
-            }
-        }
-        
-        return dp[n];
     }
 };
 
-int stringToInteger(string input) {
-    return stoi(input);
-}
+int main(int argc, char* argv[])
+{
+    if(argc<1) return 1;
 
-int main() {
-    string line;
-    while (getline(cin, line)) {
-        int n = stringToInteger(line);
-        
-        int ret = Solution().numTrees(n);
 
-        string out = to_string(ret);
-        cout << out << endl;
-    }
-    return 0;
+
+
 }
