11d9ba0f9c56b4c1177216bbe25df2e19f1e9037
CTY <chitung.yue@gmail.com>
2019-06-07 16:48:19 -0400

    Updated 70_Climbing_Stairs.cpp.

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -1,9 +1,17 @@
+/*
+ * @Author: Zidong Yu
+ * @Email: chitung.yue@gmail.com
+ * @Date: 2019-06-07 15:52:39
+ * @LastEditors: Zidong Yu
+ * @LastEditTime: 2019-06-07 16:47:51
+ * @Description: To be added.
+ */
+
 #include <iostream>
 using namespace std;
 
 class Solution_1 {
 public:
-    
     //Iteration Approach
     //Time Complexity: O(1)
     //Space Complexity: O(1)
@@ -22,14 +30,31 @@ public:
             f1=f2;
             f2=f3;
         }
-        
+
         return f2;
     }
 };
 
-
-
-
+class Solution_2
+{
+public:
+    //Brute Force (Recursion Approach)
+    //Time Complexity: O(2^n)
+    //Space Complexity: O(n)
+    int climbStairs(int n)
+    {
+        return climb_helper(0, n);
+    }
+    
+    int climb_helper(int i, int n)
+    {
+        if(i>n) return 0;
+        
+        if(i==n) return 1;
+        
+        return climb_helper(i+1,n)+climb_helper(i+2,n);
+    }
+}
 
 int stringToInteger(string input) {
     return stoi(input);
