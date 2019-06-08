d2d5a04570018e6b8c81ca99156a56bd850dcf25
CTY <chitung.yue@gmail.com>
2019-06-08 00:45:56 -0400

    Updated 70_Climbing_Stairs.cpp.

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -3,8 +3,9 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-06-07 15:52:39
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-06-07 16:52:46
- * @Description: To be added.
+ * @LastEditTime: 2019-06-08 00:45:26
+ * @Description: This is a very good example to 
+ * learn recursion, DP and more.
  */
 
 #include <iostream>
@@ -13,7 +14,7 @@ using namespace std;
 class Solution_1 {
 public:
     //Iteration Approach
-    //Time Complexity: O(1)
+    //Time Complexity: O(n)
     //Space Complexity: O(1)
     int climbStairs(int n) 
     {
@@ -39,7 +40,7 @@ class Solution_2
 {
 public:
     //Brute Force (Recursion Approach)
-    //Time Complexity: O(2^n)
+    //Time Complexity: O(2^n) ??? How?
     //Space Complexity: O(n)
     int climbStairs(int n)
     {
@@ -54,7 +55,54 @@ public:
         
         return climb_helper(i+1,n)+climb_helper(i+2,n);
     }
-}
+};
+
+class Solution_3
+{
+public:
+    //Recursion with Memoization
+    //Time Complexity: O(n) ??? Why
+    //Space Complexity: O(n)
+    int climbStairs(int n)
+    {
+        int memo[n+1];
+        return climb_helper(0, n, memo);
+    }
+    
+    int climb_helper(int i, int n, int memo[])
+    {
+        if(i>n) return 0;
+        if(i==n) return 1;
+        if(memo[i]>0) return memo[i];
+        
+        memo[i]=climb_helper(i+1,n,memo)+climb_helper(i+2,n,memo);
+        
+        return memo[i];
+    }
+};
+
+class Solution_4
+{
+public:
+    //Dynamic Programming
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    int climbStairs(int n)
+    {
+        if(n==1) return 1;
+        int dp[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+
+};
 
 int stringToInteger(string input) {
     return stoi(input);
@@ -65,7 +113,7 @@ int main() {
     while (getline(cin, line)) {
         int n = stringToInteger(line);
         
-        int ret = Solution_1().climbStairs(n);
+        int ret = Solution_4().climbStairs(n);
 
         string out = to_string(ret);
         cout << out << endl;
