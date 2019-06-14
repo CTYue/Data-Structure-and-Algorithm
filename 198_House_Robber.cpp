e8bfdc80b7cd1a0b89bc7eef38aa967f8db16129
CTY <chitung.yue@gmail.com>
2019-06-14 17:13:25 -0400

    modified:   198_House_Robber.cpp

--- 198_House_Robber.cpp
+++ 198_House_Robber.cpp
@@ -3,20 +3,31 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-06-13 23:42:38
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-06-14 16:04:07
- * @Description: 尚未理解！
+ * @LastEditTime: 2019-06-14 17:12:03
+ * @Description: This is a very typical 
+ * DP problem. The formula for maximum
+ * robbery treasure is Tmax=max(f(k-1),f(k-2)+t[k])
+ * In such a methodology, DP solution can be
+ * applied, specifically, this problem requires only
+ * the maximum value (not the entire path), O(1) space
+ * complexity can be implemented.
  */
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 class Solution {
 public:
+    //题意：找到不连续元素的最大和
     //Dynanmic Programming
-    //Time Complexity:
-    //Space Complexity:
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)(pre_Max and cur_Max)
+    //Space Complexity: O(n)(vector<int> res(len,0))
+    //
     int rob(vector<int>& nums) 
     {
         if(nums.size()==0)
@@ -26,23 +37,44 @@ public:
             return nums[0];
         
         int len=nums.size();
-        int pre_Max=0;
-        int cur_Max=0;
+        // int pre_Max=0;
+        // int cur_Max=0;
+        vector<int> res(len,0);
         
-         for(int i=0;i<len;i++)
+        // for(int i=0;i<len;i++)
+        // {
+        //     //到当前的最大值
+        //     int temp=cur_Max;
+        //     std::cout << "temp = " << temp << std::endl;
+        //     //pre_Max: f(k-2)
+        //     //cur_Max: f(k-1)
+        //     //cur_Max=max(f(k-2)+nums[i],f(k-1))
+        //     cur_Max=std::max(pre_Max+nums[i],cur_Max);
+        //     pre_Max=temp;//到前一个的最大值
+        //     std::cout << "pre_Max = " << pre_Max << std::endl;
+        //     std::cout << "cur_Max = " << cur_Max << std::endl;
+        // }
+        
+
+        for(int i=0;i<len;i++)
         {
-            int temp=cur_Max;//到当前的最大值
-            std::cout << "temp = " << temp << std::endl;
-            //pre_Max: f(k-2)
-            //cur_Max: f(k-1)
-            //cur_Max=max(f(k-2)+nums[i],f(k-1))
-            cur_Max=std::max(pre_Max+nums[i],cur_Max);
-            pre_Max=temp;//到前一个的最大值
-            std::cout << "pre_Max = " << pre_Max << std::endl;
-            std::cout << "cur_Max = " << cur_Max << std::endl;
+            if(i==0)
+                res[i]=nums[0];
+            
+            if(i==1)
+                res[i]=std::max(nums[i-1],nums[i]);
+                
+            if(i>=2)
+            {
+                std::cout << "res[i-1] = " << res[i-1] << std::endl;
+                std::cout << "res[i-2] = " << res[i-2] << std::endl;
+                res[i]=std::max(res[i-1],res[i-2]+nums[i]);
+            }
         }
         
-        return cur_Max;
+        std::cout << "res[len] = " << res[len-1] << std::endl;
+        
+        return res[len-1];
     }
 };
 
