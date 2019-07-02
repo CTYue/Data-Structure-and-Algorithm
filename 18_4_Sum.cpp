b63f1c1327f3910a2e6d39dc560f996b3f37403e
CTY <chitung.yue@gmail.com>
2019-07-01 20:17:32 -0400

    modified:   18_4_Sum.cpp

--- 18_4_Sum.cpp
+++ 18_4_Sum.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-07-01 19:33:27
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-07-01 20:01:36
+ * @LastEditTime: 2019-07-01 20:17:20
  * @Description: To be added.
  */
 
@@ -11,11 +11,14 @@
 #include <vector>
 
 using namespace std;
+
 class Solution {
 public:
-    vector<vector<int> > fourSum(vector<int>& nums, int target) 
+    //Time Complexity: ???
+    //Space Complexity: O(n)
+    vector<vector<int>> fourSum(vector<int>& nums, int target) 
     {
-        vector<vector<int> > res;
+        vector<vector<int>> res;
         if(nums.empty())    return res;
         int len=nums.size();
         
@@ -33,16 +36,19 @@ public:
             //左右两边同时收紧一个位置
             for(int j=i+1;j<len-2;j++)
             {
-                //没懂!!!
+                //没懂，为什么j>i+1???
                 if(j>i+1 && nums[j]==nums[j-1]) continue;
-                //
+                //若右边4项之和大于target，则break
                 if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
-                //
+                //如果左边4项之和小于target，则继续
                 if(nums[i]+nums[j]+nums[len-2]+nums[len-1]<target) continue;
-                
+                //定义左边界为j+1, 右边界为len-1
                 int left=j+1, right=len-1;
-                while(left<right)//当左小于右边时
+                //当左边小于右边时
+                //
+                while(left<right)
                 {
+                  //这里没理解！
                   int sum=nums[left]+nums[right]+nums[i]+nums[j];
                   if(sum<target)  left++;
                   else if(sum>target)  right--;
