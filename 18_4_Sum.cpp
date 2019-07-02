94c19575c247b1889b4328811e506511ac1c1546
CTY <chitung.yue@gmail.com>
2019-07-01 20:53:43 -0400

    modified:   18_4_Sum.cpp

--- 18_4_Sum.cpp
+++ 18_4_Sum.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-07-01 19:33:27
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-07-01 20:47:17
+ * @LastEditTime: 2019-07-01 20:53:36
  * @Description: To be added.
  */
 
@@ -14,9 +14,8 @@ using namespace std;
 
 class Solution {
 public:
-    //这个解法我没懂!
-    //Time Complexity: ???
-    //Space Complexity: O(n)
+    //Time Complexity: 
+    //Space Complexity: 
     vector<vector<int> > fourSum(vector<int>& nums, int target) 
     {
         vector<vector<int> > res;
@@ -26,41 +25,8 @@ public:
         if(len<4)   return res;
         sort(nums.begin(),nums.end());
         
-        for(int i=0;i<len-3;i++)
-        {
-            //若找到两个相同的项，则继续
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            //由于已经排序，所以若右边4项相加大于target，则直接break
-            if(nums[i]+nums[i+1]+nums[i+2]+nums[i+3]>target) break;
-            //若左边4项相加之和小于target，则continue
-            if(nums[i]+nums[len-3]+nums[len-2]+nums[len-1]<target) continue;
-            //左右两边同时收紧一个位置
-            for(int j=i+1;j<len-2;j++)
-            {
-                //没懂，为什么j>i+1???
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                //若右边4项之和大于target，则break
-                if(nums[i]+nums[j]+nums[j+1]+nums[j+2]>target) break;
-                //如果左边4项之和小于target，则继续
-                if(nums[i]+nums[j]+nums[len-2]+nums[len-1]<target) continue;
-                //定义左边界为j+1, 右边界为len-1
-                int left=j+1, right=len-1;
-                //当左边小于右边时
-                //为什么这里要用while?
-                while(left<right)
-                {
-                  int sum=nums[left]+nums[right]+nums[i]+nums[j];//这里没理解！
-                  if(sum<target)  left++;//为什么？
-                  else if(sum>target)  right--;//为什么？
-                  else
-                    {
-                        res.push_back(vector<int >{nums[i],nums[j],nums[left],nums[right]});
-                        do{left++;}while(nums[left]==nums[left-1]&&left<right);
-                        do{right--;}while(nums[right]==nums[right+1]&&left<right);
-                    }
-                }
-            }
-        }
+                
+        
         return res;
     }
 };
