6224cb083c4249e5b29facb4d99d7d40dd725af5
CTY <chitung.yue@gmail.com>
2019-07-08 14:29:45 -0400

    modified:   1109_Corporate_Flight_Bookings.cpp 	new file:   370_Range_Addition.cpp

--- 1109_Corporate_Flight_Bookings.cpp
+++ 1109_Corporate_Flight_Bookings.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-07-08 11:06:50
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-07-08 13:58:46
+ * @LastEditTime: 2019-07-08 14:27:26
  * @Description: To be added.
  * @AC: NO
  * @Related: 370@Leetcode
@@ -18,7 +18,7 @@ class Solution {
 public:
     //这道题有时间复杂度要求！
     //时间复杂度必须小于O(n^2)
-    //使用线段树解决
+    //使用线段树解决???
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) 
     {
         vector<int> res;
@@ -28,31 +28,28 @@ public:
           res.push_back(bookings[0][2]);
           return res;
         }
-        
-        res.resize(n+1,0);
+
+        res.resize(n,0);
         for(auto& v: bookings)
         {
             res[v[0]-1]+=v[2];
             // std::cout << "res[v[1]] = " << res[v[1]] << std::endl;
             std::cout << "v[0] = " << v[0] << std::endl;
             // std::cout << "v[1] = " << v[1] << std::endl;
-            //这里没理解！
-            //如果v[1]==n呢？
             if(v[1]<n) res[v[1]] -= v[2];
         }
-        
         
         for(int i=1;i<n;i++)
             res[i]+=res[i-1];
         
-        return {res.begin(), res.end() - 1};
+        return res;
     }
-};
+    };
 
 
 int main(int argc, char* argv[])
 {
     //TODO
-
+    
 
 }
---
+++ 370_Range_Addition.cpp
@@ -0,0 +1,46 @@
+/*
+ * @Author: Zidong Yu
+ * @Email: chitung.yue@gmail.com
+ * @Date: 2019-07-08 14:27:40
+ * @LastEditors: Zidong Yu
+ * @LastEditTime: 2019-07-08 14:29:28
+ * @Description: To be added.
+ * @AC: YES
+ * @Related: 1109@Leetcode
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) 
+    {
+        vector<int> res(length+1,0);
+                
+        for(auto& v:updates)
+        {
+            if(v[0]<length && v[1]<length)
+            {
+                res[v[0]]+=v[2];
+                res[v[1]+1]-=v[2];//
+            }
+        }
+        
+        for(int i=1;i<length;i++)
+        {
+            res[i]+=res[i-1];
+        }
+        
+        return {res.begin(),res.end()-1};
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    //TODO
+
+    
+}
