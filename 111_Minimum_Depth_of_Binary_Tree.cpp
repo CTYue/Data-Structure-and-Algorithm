442cc1a94136eec87399c4dfeb5d249638d287b6
CTY <chitung.yue@gmail.com>
2019-07-15 16:14:14 -0400

    modified:   111_Minimum_Depth_of_Binary_Tree.cpp 	modified:   a.out

--- 111_Minimum_Depth_of_Binary_Tree.cpp
+++ 111_Minimum_Depth_of_Binary_Tree.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-07-15 14:51:20
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-07-15 16:08:16
+ * @LastEditTime: 2019-07-15 16:14:08
  * @Description: To be added.
  * @AC: Solution_1: Yes
  *      Solution_2: 
@@ -25,18 +25,20 @@ class Solution_1 {
 public:
     //DFS
     //Time Complexity: 
-    //Space Complexity:
+    //Space Complexity: 
+    //没怎么理解！
     int minDepth(TreeNode* root) 
     {
         if(root==nullptr)   return 0;
         if(root->left==nullptr && root->right==nullptr) return 1;
         
-        int min_depth=INT_MAX;
-        if(root->left!=nullptr)
-            min_depth=std::min(minDepth(root->left),min_depth);
+        int min_depth=INT_MAX;//
         
-        if(root->right!=nullptr)
-            min_depth=std::min(minDepth(root->right),min_depth);
+        //为什么只访问了3和20？？？
+        std::cout << "root->val = " << root->val << std::endl;
+        
+        if(root->left!=nullptr)     min_depth=std::min(minDepth(root->left),min_depth);
+        if(root->right!=nullptr)    min_depth=std::min(minDepth(root->right),min_depth);
         
         return min_depth+1;
     }    
@@ -45,11 +47,16 @@ public:
 class Solution_2
 {
 public:
+    //Time Complexity: 
+    //Space Complexity: 
     int minDepth(TreeNode *root) 
     {
-        if(!root) return 0;
-        if(!root->left) return 1 + minDepth(root->right);
-        if(!root->right) return 1 + minDepth(root->left);
+        if(root==nullptr) return 0;
+        std::cout << "root->val = " << root->val << std::endl;
+
+        if(root->left==nullptr) return 1 + minDepth(root->right);
+        if(root->right==nullptr) return 1 + minDepth(root->left);
+        
         return 1+min(minDepth(root->left),minDepth(root->right));
     }
 };
@@ -117,7 +124,7 @@ int main() {
     while (getline(cin, line)) {
         TreeNode* root = stringToTreeNode(line);
         
-        int ret = Solution().minDepth(root);
+        int ret = Solution_2().minDepth(root);
 
         string out = to_string(ret);
         cout << out << endl;
