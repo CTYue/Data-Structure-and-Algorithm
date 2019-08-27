d641819e92aabbe841f50b51682cc1c4335317bb
CTY <chitung.yue@gmail.com>
2019-08-26 22:03:32 -0400

    modified:   1086_High_Five.cpp

--- 1086_High_Five.cpp
+++ 1086_High_Five.cpp
@@ -3,27 +3,53 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-08-24 00:21:12
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-08-24 00:22:53
+ * @LastEditTime: 2019-08-26 22:02:54
  * @Description: To be added.
  * @AC: Yes
  */
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 class Solution {
 public:
-    vector<vector<int> > highFive(vector<vector<int> >& items) 
+    vector<vector<int>> highFive(vector<vector<int>>& items) 
     {
-        vector<vector<int> > res;
-        map<int, vector<int> > map;
-        for(auto& v: items) map[v[0]].push_back(v[1]);
-        for(auto& [i,v]: map)//[i, v] is a C++ 17 feature, don't use it in interview!
+        vector<vector<int>> res;
+        unordered_map<int, vector<int>> hash;
+        
+        for(auto item:items)
         {
-            partial_sort(v.begin(), v.begin()+5, v.end(), greater<int>());//最后一个参数怎么理解？
-            res.push_back({i, (v[0] + v[1] + v[2] + v[3] + v[4]) / 5 });
+            if(hash.find(item[0])==hash.end())  hash[item[0]]=vector<int>();
+            if(hash[item[0]].size()<5)  hash[item[0]].push_back(item[1]);
+            else
+            {
+                int index=0;
+                //这是什么意思?
+                for(int i=1;i<5;i++)
+                    if(hash[item[0]][i]<hash[item[0]][index])   index=i;
+                //这又是什么意思?
+                if(hash[item[0]][index]<item[1])    hash[item[0]][index]=item[1];//
+            }
         }
+        
+        for(auto it=hash.begin();it!=hash.end();it++)
+        {
+            int id=it->first;
+            vector<int> scores=it->second;
+            double average=0;
+            for(int i=0;i<5;i++)    average+=scores[i];
+            average/=5.0;
+            std::cout << "id = " << id << std::endl;
+            std::cout << "average = " << average << std::endl;
+            vector<int> student;
+            student.push_back(id);
+            student.push_back(average);
+            res.push_back(student);
+        }
+        
+        std::sort(res.begin(),res.end());
         return res;
     }
 };
