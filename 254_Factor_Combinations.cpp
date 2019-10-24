79e27221c72980ce3fef3db9b742f38a5e359c11
CTY <chitung.yue@gmail.com>
2019-10-23 21:06:56 -0400

    modified:   254_Factor_Combinations.cpp

--- 254_Factor_Combinations.cpp
+++ 254_Factor_Combinations.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-10-18 03:24:08
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-10-23 20:53:04
+ * @LastEditTime: 2019-10-23 21:06:51
  * @Description: To be added.
  */
 
@@ -14,35 +14,35 @@
 using namespace std;
 class Solution {
 public:
-    //DFS
     //Time Complexity: O(logN)
-    //Space Complexity: O(N)
+    //Space Complexity: O(logN) (Worst case)
     vector<vector<int> > res;
     vector<vector<int> > getFactors(int n) 
     {
         vector<int> vec;
-        dfs(n, 2, vec);
-        return res;  
+        if(n<2) return res;
+        
+        dfs(n,2,vec);
+        return res;
     }
-    
     void dfs(int n, int factor, vector<int> vec)
     {
-        if(factor>sqrt(n)) return;
+        if(factor>sqrt(n))  return;
         
-        dfs(n,factor+1,vec);
+        dfs(n, factor+1,vec);
         
         if(n%factor==0)
-        {
+        {          
             vec.push_back(factor);
             
-            dfs(n/factor, factor,vec);
-
+            dfs(n/factor, factor, vec);
+            
+            //push back the last factor
             vec.push_back(n/factor);
             res.push_back(vec);
-        }
+        }       
     }
 };
-
 
 int main(int argc, char* argv[])
 {
