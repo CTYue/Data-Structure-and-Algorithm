90041b80e3e57ab538f4d70dd36bb6823fe03d49
CTY <chitung.yue@gmail.com>
2019-10-23 23:58:07 -0400

    modified:   17_Letter_Combinations_of_a_Phone_Number.cpp

--- 17_Letter_Combinations_of_a_Phone_Number.cpp
+++ 17_Letter_Combinations_of_a_Phone_Number.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-05-21 20:11:02
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-10-23 23:16:28
+ * @LastEditTime: 2019-10-23 23:58:02
  * @Description: 
  * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
  * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
@@ -21,10 +21,10 @@
 using namespace std;
 
 class Solution {
-//Backtracking
-//Time Complexity:  
-//Space Complexity: O(1) (Constant, not related to input) 
 private:
+    //Backtracking
+    //Time Complexity: O(3^N*4^M)
+    //Space Complexity: O(3^N*4^M)
     unordered_map<int, string> map={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
     vector<string> res;
 public:
@@ -51,13 +51,14 @@ public:
                 cout<<"local = " <<local<<endl;
                 local.push_back(map[digits[index]-'0'][i]);
                 backtracking(digits, local, index+1);
+                
+                cout<<"local.back() = "<<local.back()<<endl;
                 local.pop_back();//清空本级的local
             }
         }
             
     }
 };
-
 
 
 //Test Stub
