a4e08558dbcf027820e3eb6f44a2a787dd1696db
CTY <chitung.yue@gmail.com>
2019-10-28 20:22:47 -0400

    modified:   207_Course_Schedule.cpp

--- 207_Course_Schedule.cpp
+++ 207_Course_Schedule.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-08-21 21:11:28
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-10-28 11:15:01
+ * @LastEditTime: 2019-10-28 20:22:41
  * @Description:
  * There are a total of n courses you have to take, labeled from 0 to n-1.
  * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, 
@@ -27,23 +27,20 @@ using namespace std;
 class Solution {
 public:
     //BFS
-    //Time Complexity: O(n)
-    //Space Complexity: O(n)
+    //Time Complexity: O(Vertex+Edge)
+    //Space Complexity: 
     bool canFinish(int numCourses, vector<vector<int> >& prerequisites) 
     {        
         vector<vector<int> > graph(numCourses);//Adjacent list
         vector<int> degrees(numCourses,0);//指向某点的in-degree个数
         
         for(int i=0;i<prerequisites.size();i++)
-        {
             graph[prerequisites[i][1]].push_back(prerequisites[i][0]);//临近index的vector of nodes.对于该有向图，graph[i]                                                                         //[j]表示i指向j(先序课指向课程)
-        }
         
         for(auto v:graph)
-        {
             for(int node:v) degrees[node]++;//degrees[node]==1, 指向node的in-degree的数量++
-        }        
-        
+
+
         //Topological Sorting
         for(int i=0;i<numCourses;i++)
         {
