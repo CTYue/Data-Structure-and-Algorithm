cc1fcccb7f73b8a9fb4f7e6d2bdbc5af16f842bb
CTY <chitung.yue@gmail.com>
2019-11-06 18:12:08 -0500

    modified:   269_ Alien_Dictionary.cpp

--- 269_ Alien_Dictionary.cpp
+++ 269_ Alien_Dictionary.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-11-05 23:12:08
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-11-06 16:14:05
+ * @LastEditTime: 2019-11-06 18:12:00
  * @Description:
  * There is a new language which we don't know how the 
  * letters were ordered, you are provided with a dictionary
@@ -37,6 +37,22 @@
  * ] 
  * Output: "" 
  */
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
 
 
 
+
+
+
+
+int main(int argc, char* argv)
+{
+
+
+
+}
