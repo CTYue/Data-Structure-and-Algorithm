5095347afc7a129af8d073cb0697994d49d6dbac
CTY <chitung.yue@gmail.com>
2019-11-06 18:10:42 -0500

    new file:   269_ Alien_Dictionary.cpp

---
+++ 269_ Alien_Dictionary.cpp
@@ -0,0 +1,42 @@
+/*
+ * @Author: Zidong Yu
+ * @Email: chitung.yue@gmail.com
+ * @Date: 2019-11-05 23:12:08
+ * @LastEditors: Zidong Yu
+ * @LastEditTime: 2019-11-06 16:14:05
+ * @Description:
+ * There is a new language which we don't know how the 
+ * letters were ordered, you are provided with a dictionary
+ * like below.
+ * 
+ * Example 1:
+ * Input:
+ * [
+ * "wrt",
+ * "wrf",
+ * "er",
+ * "ett",
+ * "rftt"
+ * ]
+ * Output: "wertf"
+ *
+ * Example 2:
+ * Input:
+ * [
+ *  "z",
+ *  "x"
+ * ]
+ * Output: "zx"
+ * 
+ * Example 3:
+ * Input:
+ * [
+ *  "z",
+ *  "x",
+ *  "z"
+ * ] 
+ * Output: "" 
+ */
+
+
+
