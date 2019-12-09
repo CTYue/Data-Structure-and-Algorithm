050abbb2d08e4107ee39ccea9aa37dd1d16d975b
CTY <chitung.yue@gmail.com>
2019-12-08 19:54:18 -0500

    modified:   1283_Find_the_Smallest_Divisor_Given_a_Threshold.cpp 	modified:   a.out

--- 1283_Find_the_Smallest_Divisor_Given_a_Threshold.cpp
+++ 1283_Find_the_Smallest_Divisor_Given_a_Threshold.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-12-08 17:41:33
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-12-08 17:42:43
+ * @LastEditTime: 2019-12-08 19:53:50
  * @Description: To be added.
  */
 #include <iostream>
@@ -21,19 +21,21 @@ public:
         //如何找到这个divisor???
         //Binary Search?
         //每个非整数结果均入到与之最接近的整数
-        //如何实现?
-        //threshold怎么用？
         //找到一个合理的divisor，使得每个值除以divisor之后的sum与threshold最接近
-        int left = 1, right = 1e6, m, sum;//1e6: 1*10^6
+        int left = 1, right = 1e6, divisor=0, sum=0;//1e6: 1*10^6
         while (left < right) 
         {
-            m = (left + right) / 2, sum = 0;//什么意思???
-            for (int i : nums)
-                sum += (i + m - 1) / m;//什么意思？
+            //
+            divisor = (left + right) / 2, sum = 0;
+            for (int n : nums)  sum += (n + divisor - 1) / divisor;//用这种方式使得每个结果都向上取整, 要记住！
+            //如果所得sum大于阈值，，此时divisor选得过小，要增加左边界
+            //更新左边界
             if (sum > threshold)
-                left = m + 1;
+                left = divisor + 1;
+            //若所得sum小于阈值, ，divisor选得太大，需要减小右边界
+            //更新右边界
             else
-                right = m;
+                right = divisor;//这里处理得很巧妙，还没怎么理解
         }
         
         return left;
