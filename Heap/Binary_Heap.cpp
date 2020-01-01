35d7456e385ff6a17c228ba8300b203ced8a229b
CTY <chitung.yue@gmail.com>
2019-12-31 20:09:19 -0500

    modified:   Binary_Heap.cpp 	modified:   a.out

--- Heap/Binary_Heap.cpp
+++ Heap/Binary_Heap.cpp
@@ -3,16 +3,14 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-12-30 21:35:56
  * @LastEditors  : Zidong Yu
- * @LastEditTime : 2019-12-31 01:56:41
+ * @LastEditTime : 2019-12-31 20:09:08
  * @Description: A C++ program to demonstrate common Binary Heap Operations.
  */
 
 #include<iostream> 
-#include<climits> 
 using namespace std; 
 
-// A utility function to swap two elements 
-void swap(int *x, int *y) 
+void swap(int *x, int *y)
 { 
 	int temp = *x; 
 	*x = *y; 
@@ -20,22 +18,24 @@ void swap(int *x, int *y)
 }
 
 class MaxHeap 
-{ 
+{
+public: 
 	int *harr;
 	int capacity;//For array set up
 	int heap_size;
-public: 
-	MaxHeap(int capacity); 
 
+	MaxHeap(int capacity); 
+    
 	void MaxHeapify(int i); 
 
+    //Heap array starts from index 0
 	int parent(int i) { return (i-1)/2; }
 	int left(int i) { return (2*i + 1); }
 	int right(int i) { return (2*i + 2); }
 
 	int extractMax(); 
 
-	void IncreaseKey(int i, int new_val); 
+	void increaseKey(int i, int new_val); 
 
 	int getMax() { return harr[0]; }
 
@@ -43,6 +43,8 @@ public:
 
 	// Inserts a new key 'k' 
 	void insertKey(int k);  //
+
+    void print_arr();
 }; 
 //Complexity Analysis:
 //Time Complexity:
@@ -59,33 +61,44 @@ MaxHeap::MaxHeap(int cap)
     capacity=cap;
     heap_size=0;
     harr=new int[cap];
+    memset(harr,0,cap);
 }
 
 void MaxHeap::MaxHeapify(int i)
 {
+    //
     int l=left(i);//return left child of i
+    //
     int r=right(i);//return right child of i
+    //
     int largest=i;//largest equals to i by default, 
     
     if(l<heap_size && harr[l]>harr[i]) largest=l;
-    else largest=i;
     
     if(r<heap_size && harr[r]>harr[largest]) largest=r;
     
-    if(largest!=1)  swap(&harr[i], &harr[largest]);
-    
-    MaxHeapify(largest);
+    if(largest!=i)  
+    {
+        swap(&harr[i], &harr[largest]);
+        MaxHeapify(largest);
+    }
 }
 
 int MaxHeap::extractMax()
 {
-    if(heap_size<1) cout<<"Heap Overflow!"<<endl;
-    int max=harr[1];//这里是1还是0？
-    harr[1]=harr[heap_size-1];//swap head and tail
+    if (heap_size <= 0) return INT_MIN; 
+    if (heap_size == 1) 
+    { 
+        heap_size--; 
+        return harr[0]; 
+    } 
+    
+    int max=harr[0];
+    harr[0]=harr[heap_size-1];
     
     heap_size--;
-    MaxHeapify(1);//Re-heapify from root
-    
+
+    MaxHeapify(0);
     return max;
 }
 
@@ -93,56 +106,61 @@ void MaxHeap::insertKey(int k)
 {
     if(heap_size==capacity)
     {
-        cout<<"Heap is full!"<<endl;
+        cout<<"Heap Size: "<<heap_size<<" Capacity: "<<capacity<<endl<<"Heap is full!"<<endl;
         return;
     }
-
+    //Push back new key to the tail of the array
     heap_size++;
-    harr[heap_size]=INT_MIN;//将末尾元素设置为负无穷大, 为什么？
+    harr[heap_size-1]=k;
+    int index=heap_size-1;
     
-    //第二个参数为什么是INT_MAX???
-    IncreaseKey(k, INT_MAX);
+    while(index>0 && harr[parent(index)]<harr[index])
+    {
+        swap(&harr[index], &harr[parent(index)]);
+        index=parent(index);
+    }
 }
 
-// Insert an element into the heap
-void MaxHeap::IncreaseKey(int i, int new_val)
-{
-    if(new_val<harr[i]) cout<<"Error, new value is too small."<<endl;
-    
-    //将i所在的值设置为新值
+void MaxHeap::increaseKey(int i, int new_val)
+{   
     harr[i]=new_val;
 
-    //当i未越界且母节点小于子节点时
-    while(i>1 && harr[parent(i)]<harr[i])
+    while(i!=0 && harr[parent(i)]<harr[i])
     {
         swap(&harr[i], &harr[parent(i)]);
         i=parent(i);
     }
 }
 
-
+//Delete key at index i.
 void MaxHeap::deleteKey(int i)
 {
-    IncreaseKey(i, INT_MAX);
-    extractMax();
+    increaseKey(i, INT_MAX);
+    extractMax();//Extract the key
 }
+
+void MaxHeap::print_arr()
+{
+    for(int i=0;i<heap_size;i++)  cout<<harr[i]<<" ";
+    cout<<endl;
+}
+
 
 // Driver program to test above functions 
 int main() 
 { 
-	MaxHeap h(11); 
+	MaxHeap h(256); 
 	h.insertKey(3); 
 	h.insertKey(2); 
-	// h.deleteKey(1); 
+    h.insertKey(666); 
 	h.insertKey(15); 
 	h.insertKey(5); 
 	h.insertKey(4); 
 	h.insertKey(45); 
-	// cout << h.extractMax() << " "; 
-	cout << h.getMax() << " "; 
-	// h.IncreaseKey(2, 1); 
-	// cout << h.getMax()<<endl; 
+    h.print_arr();
+    cout<<"Max element: ";
+    cout<<h.getMax()<<endl;
+    for(int i=0;i<h.heap_size;i++)  cout<<h.extractMax()<<endl;
 
-    cout<<endl;
 	return 0; 
 } 
