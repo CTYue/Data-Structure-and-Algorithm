3187b70d72e43d6ddc80f9144f12939a056c14e9
CTY <chitung.yue@gmail.com>
2020-01-25 21:17:21 -0500

    modified:   1169_Invalid_Transactions.cpp

--- 1169_Invalid_Transactions.cpp
+++ 1169_Invalid_Transactions.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2020-01-25 03:54:47
  * @LastEditors  : Zidong Yu
- * @LastEditTime : 2020-01-25 03:56:07
+ * @LastEditTime : 2020-01-25 21:17:05
  * @Description: To be added.
  */
 #include <iostream>
@@ -18,42 +18,42 @@ class Solution {
 public:
     vector<string> invalidTransactions(vector<string>& transactions) 
     {
-        // vector<string> res;
-        unordered_set<string> res;
-        unordered_map<string, vector<vector<string> > > map;
+        unordered_set<string> set;///make sure there's no duplicate items
+        unordered_map<string, vector<vector<string> > > map;//dictionary using name as index
         
-        for(auto str: transactions)
+        for(string str: transactions)
         {
-            istringstream ss(str);//istringstream用于输入
+            istringstream ss(str);
             
-            vector<string> s(4, "");
+            //name, time, amount, city
+            vector<string> temp(4);
+            
             int i=0;
+            while(getline(ss, temp[i++], ','));//第三个参数是char
             
-            while(getline(ss, s[i++], ','));//istream& getline (istream&  is, string& str, char delim); delim是指截断符
-            //Find out amount>1000
-            if(stoi(s[2])>1000) res.insert(str);
-            //Find out time difference >= 60 min and in different cities
-            for(auto j:map[s[0]])
+            for(auto s: temp)
+                cout<<s<<" ";
+            cout<<endl;
+            
+            if(stoi(temp[2])>1000) set.insert(str);
+            
+            for(auto v: map[temp[0]])
             {
-                //当前项与map中已有项进行对比
-                //在不同的城市且时间相差60分钟以
-                if((j[3]!=s[3]) && abs(stoi(j[1])-stoi(s[1]))<=60)   
+                if(temp[3]!=v[3] && abs(stoi(temp[1])-stoi(v[1]))<=60)
                 {
-                    res.insert(j[0]+","+j[1]+","+j[2]+","+j[3]);
-                    if(res.count(str)==0) res.insert(str);//为什么要这句代码? 要将不合格的所有项都写进去！                          
+                    set.insert(str);
+                    set.insert(v[0]+","+v[1]+","+v[2]+","+v[3]);
                 }
             }
-            
-            map[s[0]].push_back({s[0],s[1],s[2],s[3]});
+                        
+            map[temp[0]].push_back(temp);
         }
         
         vector<string> ans;
-        for(auto d: res)    ans.push_back(d);
+        for(string s: set)  ans.push_back(s);
         
         return ans;
     }
-    
-    
 };
 
 int main(int argc, char* argv[])
