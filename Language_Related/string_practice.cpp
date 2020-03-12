56317fe586413399f547e6a5cd9ee8f1acfc63fc
CTY <chitung.yue@gmail.com>
2020-03-12 16:34:05 -0400

    new file:   443_String_Compression.cpp 	modified:   Binary_Search/.vscode/settings.json 	modified:   Language_Related/a.out 	modified:   Language_Related/string_practice.cpp 	modified:   a.out

--- Language_Related/string_practice.cpp
+++ Language_Related/string_practice.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2020-02-23 00:25:07
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2020-02-23 23:49:07
+ * @LastEditTime: 2020-03-12 16:12:02
  * @Description: To be added.
  */
 #include<iostream>
@@ -21,16 +21,17 @@ public:int m=222;
 
 int main()
 {
-  string A="Heo";
-  string B="ll";
-  //String insertion
-  A.insert(2,B);
-  cout<<A<<endl;
+  // string A="Heo";
+  // string B="ll";
+  // //String insertion
+  // A.insert(2,B);
+  // cout<<A<<endl;
 
-  insert_string s;
-  cout<<s.m<<endl;
+  // insert_string s;
+  // cout<<s.m<<endl;
 
-  //
+  int a=123;
+  cout<<std::to_string(a)[0]<<endl;//1
 
   return 1;
 }
