a3970c24aadda04b638761ad0cb9119f0263f6ca
CTY <chitung.yue@gmail.com>
2020-04-04 16:35:37 -0400

    modified:   202_Happy_Number.cpp 	new file:   53_Maximum_Subarray.cpp

--- 202_Happy_Number.cpp
+++ 202_Happy_Number.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2019-08-16 21:02:33
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2019-08-16 22:02:46
+ * @LastEditTime: 2020-04-03 19:16:41
  * @Description: To be added.
  * @AC: Solution_1: Yes, faster than 100%, less than 100%
  *      Solution_2: Yes, faster than 10%, less than 53%
@@ -14,72 +14,27 @@
 #include <unordered_set>
 
 using namespace std;
-class Solution_1 {
+class Solution_1 
+{
 public:
-    //Time Complexity:  
-    //Space Complexity: 
-    int SquareSum(int n)
+    int getNext(int n)
     {
-        int sum=0;
-        // std::cout << "==========" << std::endl;
-        // std::cout << "n = " << n << std::endl;
+         int sum=0;
+       
         while(n!=0)
         {
-            sum+=(n%10)*(n%10);
-            n/=10;
-            // std::cout << "sum = " << sum << std::endl;
+            //先余后除
+            int d=n%10;
+            cout<<"d = "<<d<<endl;
+            n=n/10;
+            cout<<"n = "<<n<<endl;
+            
+            sum+=d*d;
         }
-        // std::cout << "==========" << std::endl;
         
         return sum;
-    }
-    
-    bool isHappy(int n) 
-    {
-        int fast, slow;
-        fast=slow=n;
-        
-        do{
-            std::cout << "===========" << std::endl;
-            slow=SquareSum(slow);
-            std::cout << "slow = " << slow << std::endl;
-            fast=SquareSum(fast);
-            std::cout << "fast = " << fast << std::endl;
-            fast=SquareSum(fast);
-            std::cout << "fast = " << fast << std::endl;
-            std::cout << "===========" << std::endl;
-
-            if(fast==1) return 1;
-        }while(slow!=fast);
-        
-        return 0;
-    }
+    }   
 };
-
-
-class Solution_2 {
-public:
-    bool isHappy(int n) 
-    {
-        unordered_set<int> set;
-        
-        while(set.find(n)==set.end())
-        {
-            set.insert(n);
-            int sum=0;
-            while(n>0)
-            {
-                sum+=(n%10)*(n%10);
-                n/=10;
-            }
-            // std::cout << "sum = " << sum << std::endl;
-            n=sum;
-        }
-        
-        return !(set.find(1)==set.end());
-    }
-};
-
 
 int stringToInteger(string input) {
     return stoi(input);
---
+++ 53_Maximum_Subarray.cpp
@@ -0,0 +1,109 @@
+/*
+ * @Author: Zidong Yu
+ * @Email: chitung.yue@gmail.com
+ * @Date: 2020-04-04 16:33:48
+ * @LastEditors: Zidong Yu
+ * @LastEditTime: 2020-04-04 16:34:11
+ * @Description: To be added.
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+    //Time Complexity: O(nlogn)
+    //Space Complexity: 
+public:
+    int maxSubArray(vector<int> nums) 
+    {
+        if(nums.size()==0) return INT_MIN;
+        if(nums.size()==1) return nums[0];
+        
+        return helper(nums, 0, nums.size() - 1);
+    }
+    
+    //用的是merge sort的方法
+    int crossSum(vector<int> nums, int left, int right, int mid)
+    {
+        //这里没咋理解
+        if(left==right) return nums[left];
+        
+        int leftSum=INT_MIN,rightSum=INT_MIN;
+        int currSum=0;
+        
+        //Traverse left side
+        for(int i=mid;i>=left;--i)
+        {
+            currSum+=nums[i];
+            leftSum=std::max(leftSum, currSum);
+        }
+        
+        currSum=0;
+        //Traverse right side
+        for(int i=mid+1;i<=right;++i)
+        {
+            currSum+=nums[i];
+            rightSum=std::max(currSum, rightSum);
+        }
+
+        return leftSum+rightSum;
+    }
+    
+    int helper(vector<int> nums, int left, int right)
+    {
+        if(left==right) return nums[left];
+        
+        int mid=left+(right-left)/2;
+        
+        //这里的递归再了解下
+        int leftSum=helper(nums, left, mid);
+        int rightSum=helper(nums, mid+1, right);
+        
+        int Sum=crossSum(nums, left, right, mid);//C++中，变量和函数不能同名
+        
+        return std::max(std::max(leftSum, rightSum), Sum);
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().maxSubArray(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
