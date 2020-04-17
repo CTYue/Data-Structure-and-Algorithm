b9a57f03a4f86eb46f3929678399df048b7a93c5
CTY <chitung.yue@gmail.com>
2020-04-17 01:10:05 -0400

    modified:   325_Maximum_Size_Subarray_Sum_Equals_K.cpp 	modified:   a.out

--- 325_Maximum_Size_Subarray_Sum_Equals_K.cpp
+++ 325_Maximum_Size_Subarray_Sum_Equals_K.cpp
@@ -3,7 +3,7 @@
  * @Email: chitung.yue@gmail.com
  * @Date: 2020-04-17 00:40:27
  * @LastEditors: Zidong Yu
- * @LastEditTime: 2020-04-17 00:41:18
+ * @LastEditTime: 2020-04-17 01:09:40
  * @Description: To be added.
  */
 #include <iostream>
@@ -11,9 +11,10 @@
 #include <string>
 #include <queue>
 #include <sstream>
+#include <unordered_map>
 
 using namespace std;
-class Solution {
+class Solution_1 {
 public:
     //O(n^2) Brute force solution
     int maxSubArrayLen(vector<int>& nums, int k) 
@@ -31,6 +32,39 @@ public:
                 sum+=nums[j];
                 if(sum==k)  maxlen=std::max(maxlen, j-i+1);
             }
+        }
+        
+        return maxlen;
+    }
+};
+
+class Solution_2 {
+public:
+    //O(n) solution
+    int maxSubArrayLen(vector<int>& nums, int k) 
+    {
+        if(nums.size()==0)  return 0;
+        unordered_map<int, int> hashmap;
+        int sum=0, maxlen=0;
+        
+        for(int i=0;i<nums.size();++i)
+        {
+            sum+=nums[i];
+            
+            //If we find the first subarry which sums up to k
+            if(sum==k)  maxlen=i+1;//Update the max length
+            
+            //
+            if(hashmap.find(sum)==hashmap.end())
+                hashmap[sum]=i;
+            
+            //If we find another subarray which sums up to k (cur_sum-k==pre_sum)
+            //Then we update the maximum length
+            if(hashmap.find(sum-k)!=hashmap.end())
+            {
+                maxlen=std::max(maxlen, i-hashmap[sum-k]);
+            }
+            
         }
         
         return maxlen;
@@ -75,7 +109,7 @@ int main() {
         getline(cin, line);
         int k = stringToInteger(line);
         
-        int ret = Solution().maxSubArrayLen(nums, k);
+        int ret = Solution_2().maxSubArrayLen(nums, k);
 
         string out = to_string(ret);
         cout << out << endl;
