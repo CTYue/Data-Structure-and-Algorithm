721609fbd2b1840717a9e98663d545de602a600c
CTY <chitung.yue@gmail.com>
2020-10-27 21:21:25 -0400

    modified:   ../../../../src/com/leetcode/math/reverseInteger.java

--- Java_Implementations/src/com/leetcode/math/reverseInteger.java
+++ Java_Implementations/src/com/leetcode/math/reverseInteger.java
@@ -4,8 +4,8 @@ package com.leetcode.math;
 public class reverseInteger<StringBuild>
 {
     //Input: 54321
-    public long res = 0;
-    public int reverse(int x)
+    public static long res = 0;
+    public static int reverseInteger(int x)
     {
         recursion(x);
 
@@ -13,23 +13,30 @@ public class reverseInteger<StringBuild>
         return ans;
     }
 
-    public void recursion(int n)
+    public static void recursion(int n)
     {
         if(n==0)
             return;
 
-        int tmp = n%10;
-        n/=10;
+        int tmp = n%10;//取得当前digit
+        n/=10;//
 
-        res = res*10 + tmp;
-
+        res = res*10 + tmp;//res*10用于将上一次的digit增位，tmp是这次的本位
         if(res >= Integer.MAX_VALUE || res<=Integer.MIN_VALUE)
         {
             res=0;
             return;
         }
-
         recursion(n);
+    }
+
+    public static void main(String[] args)
+    {
+        int input = 54321;
+        System.out.println("input: "+ input);
+
+        int ans = reverseInteger(input);
+        System.out.println("output: "+ans);
     }
 
 }
